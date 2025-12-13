ad4aec089c9e56f3c1a963496abf1abf0f3a3cf8
Guilherme Henrique <teuemail@algumacoisa.com>
2025-12-12 22:07:30 -0300

    cleanup dockerfile and finalize fly deploy config

--- pix-server/index.js
+++ pix-server/index.js
@@ -191,6 +191,6 @@ console.log("🚀 Notificação enviada para Fiqon!");
 // INICIO DO SERVIDOR
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log("Servidor rodando na porta", PORT);
 });
