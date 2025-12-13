ba211f5b38ca9fed3a662deff6e303cc25f9f433
Guilherme Henrique <teuemail@algumacoisa.com>
2025-12-12 21:54:37 -0300

    fix listen address and keep machine alive

--- pix-server/index.js
+++ pix-server/index.js
@@ -189,6 +189,8 @@ console.log("🚀 Notificação enviada para Fiqon!");
 
 
 // INICIO DO SERVIDOR
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta", PORT);
 });
