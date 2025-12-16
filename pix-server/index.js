9fb925f79db344e8e751d5748d22edb3c1b2d4cc
Guilherme Henrique <teuemail@algumacoisa.com>
2025-12-16 01:30:48 -0300

    fix syntax error after merge

--- pix-server/index.js
+++ pix-server/index.js
@@ -271,8 +271,3 @@ const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log("Servidor rodando na porta", PORT);
-
-// INICIO DO SERVIDOR
-app.listen(3000, () => {
-  console.log("Servidor rodando na porta 3000");
-});
